a869291724df8184971685bbf7179fa40412b1e8
ZZIBS <58077717+ZZIBS@users.noreply.github.com>
2019-12-30 12:20:52 +0100

    Update main.cpp

--- Session/session3.1/main.cpp
+++ Session/session3.1/main.cpp
@@ -12,31 +12,32 @@ struct book{
 
 };
 
-book my_library[10];
+book my_library[5];
 
 
 void fill_library()      // declaring a void funcation
 {
-    cout <<"Let's fill the library wit some book!!"<<endl;
+    cout <<"Let's fill the library with some book!!"<<endl;
     my_library[0].book_name = "Computer science";
     my_library[1].book_name = "Maths";
     my_library[2].book_name = "English";
-
-
+    my_library[3].book_name = "Biology";
+    my_library[4].book_name = "Physics";
 
 }
 
 void print_library()
 {
-    cout <<my_library[1].book_name<<<<endl;
-
+        for(int i=0; i<5;i++){
+        cout <<my_library[i].book_name<<endl;
+        }
 }
 
 
 int main()
 {
-    fill_library();        // calling from a void funcation.
-    //cout<<my_library
+    fill_library();        // calling from a void function of fill_library
+    print_library();       //calling from another function for printing
 
-print_library();
+
 }
